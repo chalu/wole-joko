50e71117751049df4daf23b60c5aae8578e49fce
Charles Opute Odili <chaluwa@gmail.com>
2024-02-12 01:24:41 -0700

    add background audio

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,18 @@ import { init, doorsOpen } from './auditorium.js';
 
 const startApp = () => {
   init().then(() => doorsOpen());
+  
+  document.body.addEventListener('click', () => {
+    const audio = document.querySelector('audio');
+    if (!audio) return;
+
+    if (audio.currentTime <= 0 || audio.paused) {
+      audio.play();
+      return;
+    }
+
+    audio.pause();
+  });
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
